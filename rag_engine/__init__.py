3d6388e18f7c4ff874173da87c3810a98496978f
Slava Vishnyakov <bomboze@gmail.com>
2024-08-06 11:41:36 +0300

    Bump version: 0.1.1 → 0.1.2

--- rag_engine/__init__.py
+++ rag_engine/__init__.py
@@ -1,3 +1,3 @@
 from .rag import RAGEngine
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="rag_engine",
-    version="0.1.1",
+    version="0.1.2",
     author="Slava Vishnyakov",
     author_email="bomboze@gmail.com",
     description="A Retrieval-Augmented Generation (RAG) Engine for managing embeddings and similarity search",
